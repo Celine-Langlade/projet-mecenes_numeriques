51c5a825eedc1eb34e360758199d1a4fd3f0cfef
IbanGF <ueqiban@gmail.com>
2016-02-17 14:37:38 +0100

    Dev 2 creation

---
+++ themes/zerif-lite-child/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+function themeslug_enqueue_style() {
+    if ( is_child_theme() ) {
+        // load parent stylesheet first if this is a child theme
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'style.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/bootstrap.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/bootstrap-rtl.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/custom-editor-style.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/font-awesome.min.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/ie.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/pixeden-icons.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/responsive.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/style-mobile.css', false );
+	wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'css/zerif_customizer_custom_css.css', false );
+    }
+    // load active theme stylesheet in both cases
+    wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), false );
+}
+
+add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_style' );
+
+
+
+/* Ajout de la zone widget défiscalisé */
+if ( function_exists('register_sidebar') ) {
+
+    register_sidebar(array(
+		'name' => __( 'defiscalise', 'lmdn' ),
+		'id' => 'defiscalise',
+		'description' => __( 'Widget de defiscalise', 'lmdn'),
+		'before_widget' => '<div class="widget_defiscalise">',
+		'after_widget' => '</div>',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>', 
+	) );
+}
